b90e905784caf42907ea35423d76d64592e80148
Jason Cetina <jcetina@github.com>
2021-11-23 17:05:36 -0800

    enable functin

--- main.tf
+++ main.tf
@@ -131,13 +131,13 @@ resource "azurerm_function_app" "log_pipeline_function_app" {
   storage_account_name       = azurerm_storage_account.log_pipeline_function_app_storage.name
   storage_account_access_key = azurerm_storage_account.log_pipeline_function_app_storage.primary_access_key
 
-  /*
+  
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE"       = "https://${azurerm_storage_account.log_pipeline_function_app_storage.name}.blob.core.windows.net/${azurerm_storage_container.log_pipeline_function_app_storage_container.name}/${azurerm_storage_blob.log_pipeline_storage_blob.name}${data.azurerm_storage_account_blob_container_sas.storage_account_blob_container_token.sas}",
     "FUNCTIONS_WORKER_RUNTIME"       = "python",
     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.log_pipeline_function_application_insights.instrumentation_key,
   }
-*/
+
 
   os_type = "linux"
   version = "~3"
@@ -170,12 +170,13 @@ data "azurerm_storage_account_blob_container_sas" "storage_account_blob_containe
   }
 }
 
+/*
 locals {
   az_login_command     = "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID"
   publish_code_command = "az webapp deployment source config-zip --resource-group ${azurerm_resource_group.log_pipeline.name} --name ${azurerm_function_app.log_pipeline_function_app.name} --src ${data.archive_file.log_pipeline_function.output_path}"
 }
 
-/*
+
 resource "null_resource" "az_login" {
   provisioner "local-exec" {
     command = local.az_login_command
