4e66b9dc986d59bfcc0a144afc69aadf1118b8e0
Jason Cetina <jcetina@github.com>
2021-11-30 11:16:15 -0800

    trigger update function code

--- log_pipeline_function/LogPipelineFunction/__init__.py
+++ log_pipeline_function/LogPipelineFunction/__init__.py
@@ -4,5 +4,5 @@ import azure.functions as func
 
 
 def main(msg: func.ServiceBusMessage):
-    logging.info('msg body thonk: %s',
+    logging.info('msg body whoop: %s',
                  msg.get_body().decode('utf-8'))
