d444a089ec4770e9b8e34ab0c006019d1172ab8a
Jason Cetina <jcetina@github.com>
2021-11-30 10:56:48 -0800

    update function code

--- log_pipeline_function/LogPipelineFunction/__init__.py
+++ log_pipeline_function/LogPipelineFunction/__init__.py
@@ -4,5 +4,5 @@ import azure.functions as func
 
 
 def main(msg: func.ServiceBusMessage):
-    logging.info('msg body asdf: %s',
+    logging.info('msg body asdf nba bbq: %s',
                  msg.get_body().decode('utf-8'))
