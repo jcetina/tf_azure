6c1846e234877201882a0760922853bb40176656
Jason Cetina <jcetina@github.com>
2021-12-02 14:16:47 -0800

    simplify func

--- log_pipeline_function/LogPipelineFunction/__init__.py
+++ log_pipeline_function/LogPipelineFunction/__init__.py
@@ -7,9 +7,12 @@ import azure.functions as func
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 
+logging.info('on')
+
 def main(msg: func.ServiceBusMessage):
+    logging.info('here')
     try:
-        credential = DefaultAzureCredential()
+        #credential = DefaultAzureCredential()
         # secret_client = SecretClient(vault_url="https://my-key-vault.vault.azure.net/", credential=credential)
         # secret = secret_client.get_secret("secret-name")
         logging.info('msg body: %s',
