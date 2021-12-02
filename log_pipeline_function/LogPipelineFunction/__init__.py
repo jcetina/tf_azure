71a7f189b06b8db32ac4c628e053a5960b938905
Jason Cetina <jcetina@github.com>
2021-12-02 15:06:55 -0800

    big ole reversion

--- log_pipeline_function/LogPipelineFunction/__init__.py
+++ log_pipeline_function/LogPipelineFunction/__init__.py
@@ -1,13 +1,8 @@
-import os
 import logging
 
 import azure.functions as func
 
 
-
 def main(msg: func.ServiceBusMessage):
-    #credential = DefaultAzureCredential()
-    # secret_client = SecretClient(vault_url="https://my-key-vault.vault.azure.net/", credential=credential)
-    # secret = secret_client.get_secret("secret-name")
     logging.info('msg body: %s',
-                msg.get_body().decode('utf-8'))
+                 msg.get_body().decode('utf-8'))
--- main.tf
+++ main.tf
@@ -23,29 +23,11 @@ provider "azurerm" {
   features {}
 }
 
-variable "location" {
-  type    = string
-  default = "eastus"
-}
-
-variable "hec_token_name" {
-  type    = string
-  default = "hec-token"
-}
-
-variable "hec_token_value" {
-  type      = string
-  sensitive = true
-}
-
-variable "vault_name" {
-  type    = string
-  default = "logpipelinevault"
-}
 
 resource "azurerm_resource_group" "log_pipeline" {
   name     = "LogPipelineResourceGroup"
-  location = var.location
+  location = "eastus"
+
 }
 
 
@@ -188,8 +170,6 @@ resource "azurerm_function_app" "log_pipeline_function_app" {
     "WEBSITE_RUN_FROM_PACKAGE"       = "https://${azurerm_storage_account.log_pipeline_function_app_storage.name}.blob.core.windows.net/${azurerm_storage_container.log_pipeline_function_app_storage_container.name}/${azurerm_storage_blob.log_pipeline_storage_blob.name}",
     "FUNCTIONS_WORKER_RUNTIME"       = "python",
     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.log_pipeline_function_application_insights.instrumentation_key,
-    "HEC_TOKEN_SECRET_NAME"          = var.hec_token_name,
-    "HEC_VAULT_URI"                  = azurerm_key_vault.log_pipeline_vault.vault_uri,
   }
 
   identity {
@@ -209,54 +189,6 @@ resource "azurerm_role_assignment" "log_pipeline_blob_reader" {
   principal_id         = data.azurerm_function_app.log_pipeline_function_app_data.identity.0.principal_id
 }
 
-resource "azurerm_role_assignment" "log_pipeline_service_bus_reader" {
-  scope                = azurerm_resource_group.log_pipeline.id
-  role_definition_name = "Azure Service Bus Data Receiver"
-  principal_id         = data.azurerm_function_app.log_pipeline_function_app_data.identity.0.principal_id
-}
-
-resource "azurerm_key_vault" "log_pipeline_vault" {
-  name                = var.vault_name
-  location            = azurerm_resource_group.log_pipeline.location
-  resource_group_name = azurerm_resource_group.log_pipeline.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "premium"
-
-}
-
-resource "azurerm_key_vault_access_policy" "function_app_read_policy" {
-  key_vault_id = azurerm_key_vault.log_pipeline_vault.id
-
-  tenant_id = data.azurerm_function_app.log_pipeline_function_app_data.identity.0.tenant_id
-  object_id = data.azurerm_function_app.log_pipeline_function_app_data.identity.0.principal_id
-
-  secret_permissions = [
-    "get",
-    "list"
-  ]
-}
-
-resource "azurerm_key_vault_access_policy" "key_setter_policy" {
-  key_vault_id = azurerm_key_vault.log_pipeline_vault.id
-
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azurerm_client_config.current.object_id
-
-  secret_permissions = [
-    "set",
-    "get",
-    "delete",
-    "purge",
-    "recover"
-  ]
-}
-
-resource "azurerm_key_vault_secret" "hec_token" {
-  name         = var.hec_token_name
-  value        = var.hec_token_value
-  key_vault_id = azurerm_key_vault.log_pipeline_vault.id
-}
-
 data "azurerm_function_app" "log_pipeline_function_app_data" {
   # this is a hack so that we can access the function app identity block elsewhere
   # since the azure terraform provider doesn't compute it when the resource is generated
@@ -272,5 +204,3 @@ data "archive_file" "log_pipeline_function" {
   source_dir  = "${path.module}/log_pipeline_function"
   output_path = "log_pipeline_function.zip"
 }
-
-data "azurerm_client_config" "current" {}
