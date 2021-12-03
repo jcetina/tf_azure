fab8dea7d3af5b130695ed0720199196dd5bec7d
Jason Cetina <jcetina@github.com>
2021-12-03 10:58:19 -0800

    get secret

--- log_pipeline_function/LogPipelineFunction/__init__.py
+++ log_pipeline_function/LogPipelineFunction/__init__.py
@@ -3,9 +3,15 @@ import os
 
 import azure.functions as func
 
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
 def main(msg: func.ServiceBusMessage):
     logging.info('msg body: %s',
                 msg.get_body().decode('utf-8'))
-    logging.info('hec_name {}'.format(os.environ.get('HEC_TOKEN_SECRET_NAME')))
-    logging.info('vault_uri {}'.format(os.environ.get('HEC_VAULT_URI')))
-    logging.info(str(os.environ))
+    secret_name = os.environ.get('HEC_TOKEN_SECRET_NAME')
+    vault = os.environ.get('HEC_VAULT_URI')
+    credential = DefaultAzureCredential()
+    secret_client = SecretClient(vault_url=vault, credential=credential)
+    secret = secret_client.get_secret(secret_name)
+    logging.info('{}: {}'.format(secret_name, secret))
