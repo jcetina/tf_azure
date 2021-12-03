68db36041ada3df4aee0d3df6fc54c8d3d2dc958
Jason Cetina <jcetina@github.com>
2021-12-02 17:30:03 -0800

    comment out credential foo

--- log_pipeline_function/LogPipelineFunction/__init__.py
+++ log_pipeline_function/LogPipelineFunction/__init__.py
@@ -9,6 +9,6 @@ from azure.keyvault.secrets import  SecretClient
 def main(msg: func.ServiceBusMessage):
     logging.info('msg body: %s',
                  msg.get_body().decode('utf-8'))
-    credential = DefaultAzureCredential()
+    # credential = DefaultAzureCredential()
     logging.info('key name', os.environ['HEC_TOKEN_SECRET_NAME'])
     logging.info('hec uri', os.environ['HEC_VAULT_URI'])
