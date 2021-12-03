1436f1ed8b1600ad6c1ac0e3bc38785a109142a0
Jason Cetina <jcetina@github.com>
2021-12-02 17:19:38 -0800

    add stuff to function

--- log_pipeline_function/LogPipelineFunction/__init__.py
+++ log_pipeline_function/LogPipelineFunction/__init__.py
@@ -1,8 +1,14 @@
 import logging
+import os
 
 import azure.functions as func
 
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import  SecretClient
 
 def main(msg: func.ServiceBusMessage):
     logging.info('msg body: %s',
                  msg.get_body().decode('utf-8'))
+    credential = DefaultAzureCredential()
+    logging.info('key name', os.environ['HEC_TOKEN_SECRET_NAME'])
+    logging.info('hec uri', os.environ['HEC_VAULT_URI'])
