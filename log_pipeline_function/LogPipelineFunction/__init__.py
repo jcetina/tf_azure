02262e7febd6e6c8d521be43154f24ca234f1731
Jason Cetina <jcetina@github.com>
2021-12-02 18:30:32 -0800

    print env vars

--- log_pipeline_function/LogPipelineFunction/__init__.py
+++ log_pipeline_function/LogPipelineFunction/__init__.py
@@ -1,7 +1,10 @@
 import logging
+import os
 
 import azure.functions as func
 
 def main(msg: func.ServiceBusMessage):
     logging.info('msg body: %s',
                  msg.get_body().decode('utf-8'))
+    logging.info('hec_name'.format(os.environ.get('HEC_TOKEN_SECRET_NAME')))
+    logging.info('vault_uri'.format(os.environ.get('HEC_VAULT_URI')))
