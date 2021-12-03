f4913c32cb9a2a70d3093543393ca0224244728c
Jason Cetina <jcetina@github.com>
2021-12-02 22:15:53 -0800

    print env

--- log_pipeline_function/LogPipelineFunction/__init__.py
+++ log_pipeline_function/LogPipelineFunction/__init__.py
@@ -3,8 +3,13 @@ import os
 
 import azure.functions as func
 
-def main(msg: func.ServiceBusMessage):
-    logging.info('msg body: %s',
-                 msg.get_body().decode('utf-8'))
-    logging.info('hec_name {}'.format(os.environ.get('HEC_TOKEN_SECRET_NAME')))
-    logging.info('vault_uri {}'.format(os.environ.get('HEC_VAULT_URI')))
+def main():
+    logging.info(str(os.environ))
+    foo = """
+    def main(msg: func.ServiceBusMessage):
+        logging.info('msg body: %s',
+                    msg.get_body().decode('utf-8'))
+        logging.info('hec_name {}'.format(os.environ.get('HEC_TOKEN_SECRET_NAME')))
+        logging.info('vault_uri {}'.format(os.environ.get('HEC_VAULT_URI')))
+        logging.info(str(os.environ))
+    """
