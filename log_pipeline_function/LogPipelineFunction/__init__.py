d0bda6d25fffc840123b65aa35e6a1bdd7089297
Jason Cetina <jcetina@github.com>
2021-12-03 11:38:16 -0800

    fix func

--- log_pipeline_function/LogPipelineFunction/__init__.py
+++ log_pipeline_function/LogPipelineFunction/__init__.py
@@ -3,15 +3,24 @@ import os
 
 import azure.functions as func
 
-from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
+s = """try:
+    from azure.identity import DefaultAzureCredential
+    from azure.keyvault.secrets import SecretClient
+except Exception as e:
+    logging.info(str(e))
+"""
 
 def main(msg: func.ServiceBusMessage):
     logging.info('msg body: %s',
                 msg.get_body().decode('utf-8'))
     secret_name = os.environ.get('HEC_TOKEN_SECRET_NAME')
     vault = os.environ.get('HEC_VAULT_URI')
-    credential = DefaultAzureCredential()
-    secret_client = SecretClient(vault_url=vault, credential=credential)
-    secret = secret_client.get_secret(secret_name)
-    logging.info('{}: {}'.format(secret_name, secret))
+    s2 = """
+    try:
+        credential = DefaultAzureCredential()
+        secret_client = SecretClient(vault_url=vault, credential=credential)
+        secret = secret_client.get_secret(secret_name)
+        logging.info('{}: {}'.format(secret_name, secret))
+    except Exception as e:
+        logging.info(str(e))
+    """
