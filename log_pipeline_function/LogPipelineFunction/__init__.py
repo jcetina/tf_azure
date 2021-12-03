3ee5d3e0205b4810cee7bb20bb03379342ada5c0
Jason Cetina <jcetina@github.com>
2021-12-02 22:56:43 -0800

    fix code

--- log_pipeline_function/LogPipelineFunction/__init__.py
+++ log_pipeline_function/LogPipelineFunction/__init__.py
@@ -3,13 +3,9 @@ import os
 
 import azure.functions as func
 
-def main():
+def main(msg: func.ServiceBusMessage):
+    logging.info('msg body: %s',
+                msg.get_body().decode('utf-8'))
+    logging.info('hec_name {}'.format(os.environ.get('HEC_TOKEN_SECRET_NAME')))
+    logging.info('vault_uri {}'.format(os.environ.get('HEC_VAULT_URI')))
     logging.info(str(os.environ))
-    foo = """
-    def main(msg: func.ServiceBusMessage):
-        logging.info('msg body: %s',
-                    msg.get_body().decode('utf-8'))
-        logging.info('hec_name {}'.format(os.environ.get('HEC_TOKEN_SECRET_NAME')))
-        logging.info('vault_uri {}'.format(os.environ.get('HEC_VAULT_URI')))
-        logging.info(str(os.environ))
-    """
