776e56152e8bf8574eb5c2706956ebd3e821b3b8
Jason Cetina <jcetina@github.com>
2021-12-02 20:47:17 -0800

    fix print statements

--- log_pipeline_function/LogPipelineFunction/__init__.py
+++ log_pipeline_function/LogPipelineFunction/__init__.py
@@ -6,5 +6,5 @@ import azure.functions as func
 def main(msg: func.ServiceBusMessage):
     logging.info('msg body: %s',
                  msg.get_body().decode('utf-8'))
-    logging.info('hec_name'.format(os.environ.get('HEC_TOKEN_SECRET_NAME')))
-    logging.info('vault_uri'.format(os.environ.get('HEC_VAULT_URI')))
+    logging.info('hec_name {}'.format(os.environ.get('HEC_TOKEN_SECRET_NAME')))
+    logging.info('vault_uri {}'.format(os.environ.get('HEC_VAULT_URI')))
