7cd96535444db0b0a50d115ea81ec0190754b589
Jason Cetina <jcetina@github.com>
2021-12-03 16:54:35 -0800

    updated func

--- log_pipeline_function/LogPipelineFunction/__init__.py
+++ log_pipeline_function/LogPipelineFunction/__init__.py
@@ -3,29 +3,15 @@ import os
 
 import azure.functions as func
 
-logging.info('starting')
-try:
-    from azure.identity import DefaultAzureCredential
-    from azure.keyvault.secrets import SecretClient
-except ImportError as ie:
-    logging.info("import error: {}".format(str(ie)))
-except Exception as e:
-    logging.info("other error: {}".format(str(e)))
-
-
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
 
 def main(msg: func.ServiceBusMessage):
-    try:
-        logging.info(DefaultAzureCredential)
-        logging.info(SecretClient)
-    except Exception as e:
-        logging.info("DefaultAzureCredential error: {}".format(str(e)))
     logging.info('msg body: %s',
                 msg.get_body().decode('utf-8'))
     secret_name = os.environ.get('HEC_TOKEN_SECRET_NAME')
     vault = os.environ.get('HEC_VAULT_URI')
-    s2 = """
     try:
         credential = DefaultAzureCredential()
         secret_client = SecretClient(vault_url=vault, credential=credential)
@@ -33,4 +19,3 @@ def main(msg: func.ServiceBusMessage):
         logging.info('{}: {}'.format(secret_name, secret))
     except Exception as e:
         logging.info(str(e))
-    """
