0d7c557c457a7953c3d67bc4ad77be58158c07ac
Jason Cetina <jcetina@github.com>
2021-12-06 14:42:03 -0800

    system assigned identity

--- data.tf
+++ data.tf
@@ -1,7 +1,14 @@
 data "archive_file" "function_zip" {
   source_dir  = "${path.module}/log_pipeline_function"
-  output_path = "${base64sha256(null_resource.python_dependencies.id)}-log_pipeline_function.zip"
+  output_path = "${base64sha256(null_resource.python_dependencies.id)}-${base64sha256(null_resource.set_queue_name.id)}-log_pipeline_function.zip"
   type        = "zip"
+}
+
+data "azurerm_function_app" "log_pipeline_function_app_data" {
+  # this is a hack so that we can access the function app identity block elsewhere
+  # since the azure terraform provider doesn't compute it when the resource is generated
+  name                = azurerm_function_app.log_pipeline_function_app.name
+  resource_group_name = azurerm_resource_group.log_pipeline.name
 }
 
 data "azurerm_client_config" "current" {}
