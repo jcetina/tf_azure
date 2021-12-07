9de680de102d4e2b838988ff91e8939256e8e02f
Jason Cetina <jcetina@github.com>
2021-12-07 14:56:53 -0800

    fix zip output path

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "archive_file" "function_zip" {
   source_dir  = "${path.module}/log_pipeline_function"
-  output_path = "${base64sha256(null_resource.python_dependencies.id)}-${base64sha256(null_resource.set_queue_name.id)}-log_pipeline_function.zip"
+  output_path = "${path.module}/${sha256(null_resource.python_dependencies.id)}-${sha256(null_resource.set_queue_name.id)}-log_pipeline_function.zip"
   type        = "zip"
 }
 
