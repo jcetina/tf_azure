bab81bcc88ec1a56dd6228e04f30aaaf17a18801
Jason Cetina <jcetina@github.com>
2021-12-22 12:07:41 -0800

    fix authz rule data source

--- data.tf
+++ data.tf
@@ -14,10 +14,11 @@ data "azurerm_client_config" "current" {}
 
 data "azurerm_eventhub_authorization_rule" "RootManageSharedAccessKey" {
   depends_on = [
-    azurerm_eventhub.evh_telemetry_pipeline
+    azurerm_eventhub.evh_telemetry_pipeline,
+    azurerm_eventhub_namespace.evhns_telemetry_pipeline
   ]
   for_each            = azurerm_eventhub.evh_telemetry_pipeline
-  name                = "${azurerm_eventhub_namespace.evhns_telemetry_pipeline.name}/RootManageSharedAccessKey"
+  name                = "jrctest-az-pl/RootManageSharedAccessKey"
   namespace_name      = azurerm_eventhub_namespace.evhns_telemetry_pipeline.name
   eventhub_name       = each.key
   resource_group_name = azurerm_resource_group.log_pipeline.name
