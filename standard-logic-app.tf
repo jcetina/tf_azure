056c41994abc8bff8be04809db2e953afde45c39
Jason Cetina <jcetina@github.com>
2022-01-07 17:25:04 -0800

    fix api connection

--- standard-logic-app.tf
+++ standard-logic-app.tf
@@ -67,7 +67,7 @@ resource "azurerm_logic_app_standard" "send_to_splunk" {
     "SplunkHecUrl"                            = var.splunk_endpoint
     "SplunkHecToken"                          = var.hec_token_value
     "EventQueueConnApiId"                     = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/providers/Microsoft.Web/locations/${azurerm_resource_group.log_pipeline.location}/managedApis/azurequeues"
-    "EventQueueConnectionId"                  = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.log_pipeline.name}/providers/Microsoft.Web/sites/${local.send_to_splunk_logic_app_name}/connections/EventQueueConnector"
+    "EventQueueConnectionId"                  = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.log_pipeline.name}/providers/Microsoft.Web/connections/azurequeues"
     "EventQueueConnectionKey"                 = azurerm_storage_account.storage.primary_access_key
     "EventQueueName"                          = azurerm_storage_queue.queues[local.event_output_queue].name
     "BatchWorkflowId"                         = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.log_pipeline.name}/providers/Microsoft.Web/sites/${local.send_to_splunk_logic_app_name}/workflows/BatchToSplunk"
